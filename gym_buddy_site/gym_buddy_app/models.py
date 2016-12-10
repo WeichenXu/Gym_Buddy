b7dbd8d06b834338e1b05f5b83a47ccc2ef4372d
WeichenXu <weichenxu1993@icloud.com>
2016-12-10 18:27:58 -0500

    to_string() for models

--- gym_buddy_site/gym_buddy_app/models.py
+++ gym_buddy_site/gym_buddy_app/models.py
@@ -1,16 +1,22 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 # Create your models here.
 
 # user model
+@python_2_unicode_compatible
 class User(models.Model):
     user_name = models.CharField(max_length = 200)
     user_password = models.CharField(max_length = 200)
+    def __str__(self):
+        return self.user_name
 
 # gym buddy request
+@python_2_unicode_compatible
 class Request(models.Model):
     request_time = models.DateTimeField('date published')
     # location, undecided
     requester = models.ForeignKey(User, on_delete=models.CASCADE)
-
+    def __str__(self):
+        return self.requester.user_name + self.request_time
 
