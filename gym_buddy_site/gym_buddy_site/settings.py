8dc9d172513db74514f43d4c9ccea405f668c6b1
WeichenXu <weichenxu1993@icloud.com>
2016-12-10 18:09:45 -0500

    database init script

--- gym_buddy_site/gym_buddy_site/settings.py
+++ gym_buddy_site/gym_buddy_site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gym_buddy_app.apps.GymBuddyAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
