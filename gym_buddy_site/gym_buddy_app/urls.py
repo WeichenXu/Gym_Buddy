bbd1a14d2c2ebf560566a6638e7bf125685e2386
WeichenXu <weichenxu1993@icloud.com>
2016-12-10 23:23:51 -0500

    view&url for register and login

--- gym_buddy_site/gym_buddy_app/urls.py
+++ gym_buddy_site/gym_buddy_app/urls.py
@@ -3,5 +3,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # ex:/gym_buddy/
     url(r'^$', views.index, name='index'),
+    # ex:/gym_buddy/login/username/password
+    url(r'^(?P<user_name>[\w]+)/(?P<user_password>[\w]+)/login/$', views.login, name='login'),
+    # ex:/gym_buddy/register/username/password
+    url(r'^(?P<user_name>[\w]+)/(?P<user_password>[\w]+)/register/$', views.register, name='register'),
 ]
+
--- gym_buddy_site/gym_buddy_app/views.py
+++ gym_buddy_site/gym_buddy_app/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import User, Request
+
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, app.")
+
+# Login action
+def login(request, user_name, user_password):
+    response = "Login, user_name: %s, status: %s"
+    try:
+        login_user = User.objects.get(user_name = user_name)
+        login_status = "Succeed" if login_user.user_password == user_password else "Invalid password" 
+    except User.DoesNotExist:
+        login_status = "Invalid user_name" 
+    return HttpResponse(response % (user_name, login_status) )
+
+# Register action
+def register(request, user_name, user_password):
+    response = "Register, user_name: %s, status: %s"
+    try:
+        user = User.objects.get(user_name = user_name)
+        register_status = "Duplicated user_name"
+    except User.DoesNotExist:
+        user = User(user_name = user_name, user_password = user_password)
+        user.save()
+        register_status = "Succeed"
+    return HttpResponse(response % (user_name,register_status) )
